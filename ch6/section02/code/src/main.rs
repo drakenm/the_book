dc948b6b8257cb74e80fe4f22aab084cc793c5bc
drakenm <drakenm@gmail.com>
2023-12-24 23:37:54 -0600

    coin and plus_one

--- ch6/section02/code/src/main.rs
+++ ch6/section02/code/src/main.rs
@@ -1,13 +1,44 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
     HalfDollar,
     Dollar,
 }
 
+fn value_in_cents(coin: Coin) -> u8 {
+    return match coin {
+        Coin::Penny => 1,
+        Coin:: Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+        Coin::HalfDollar => 50,
+        Coin::Dollar => 100,
+    };
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i+1),
+    }
+}
+
 fn main() {
     println!("Hello, chapter 6, section 2 code blocks!");
-    
+    value_in_cents(Coin::Quarter(UsState::Alabama));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
